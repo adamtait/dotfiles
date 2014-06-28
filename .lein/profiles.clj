d66a2ad380f6601160116a153c8fdc12a1be2aae
Adam Tait <adam@adamtait.com>
2014-06-28 14:41:21 -0700

    add new .lein/profiles.clj with lein plugins

---
+++ .lein/profiles.clj
@@ -0,0 +1,9 @@
+{
+  :user {
+     :plugins [[cider-nrepl "0.7.0-SNAPSHOT"]
+       	       [lein-cprint "1.0.0"]
+       	       [lein-deps-tree "0.2.1"]
+	       [lein-jdk-tools "0.1.1"]
+	       [lein-marginalia "0.7.1"]
+	       [lein-test-bang-bang "0.2.0"]
+	       [org.clojars.strongh/lein-init-script "1.3.1"]]}}
