bfbba52a48969411b41a0bddfadf331b150f48f3
Adam Tait <adam@adamtait.com>
2014-06-28 15:35:28 -0700

    fix version of lein-deps-tree plugin to 0.1.2

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -2,7 +2,7 @@
   :user {
      :plugins [[cider/cider-nrepl "0.7.0-SNAPSHOT"]
        	       [lein-cprint "1.0.0"]
-       	       [lein-deps-tree "0.2.1"]
+       	       [lein-deps-tree "0.1.2"]
 	       [lein-jdk-tools "0.1.1"]
 	       [lein-marginalia "0.7.1"]
 	       [lein-test-bang-bang "0.2.0"]
