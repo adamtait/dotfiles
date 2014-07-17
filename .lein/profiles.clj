abba35941601c825d7765adf18f58ef9de2e0162
Adam Tait <adam@adamtait.com>
2014-07-16 17:25:50 -0700

    add lein-localrepo to leiningen plugins

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,9 +1,9 @@
-{
-  :user {
-     :plugins [[cider/cider-nrepl "0.7.0-SNAPSHOT"]
-       	       [lein-cprint "1.0.0"]
-       	       [lein-deps-tree "0.1.2"]
-	       [lein-jdk-tools "0.1.1"]
-	       [lein-marginalia "0.7.1"]
-	       [lein-test-bang-bang "0.2.0"]
-	       [org.clojars.strongh/lein-init-script "1.3.1"]]}}
+{:user {
+    :plugins [[cider/cider-nrepl "0.7.0-SNAPSHOT"]
+              [lein-cprint "1.0.0"]
+              [lein-deps-tree "0.1.2"]
+	      [lein-jdk-tools "0.1.1"]
+	      [lein-marginalia "0.7.1"]
+	      [lein-test-bang-bang "0.2.0"]
+	      [org.clojars.strongh/lein-init-script "1.3.1"]
+	      [lein-localrepo "0.5.3"]]}}
