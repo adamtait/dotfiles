d63a3a0dcec60c8b7db7c87e18d03eb53ecd6454
Adam Tait <adam@adamtait.com>
2014-10-04 17:11:34 -0700

    add install_elpa & .path

---
+++ install_elpa.el
@@ -0,0 +1,31 @@
+(setq user-init-file "/tmp/.emacs")
+
+;; Marmalade: http://marmalade-repo.org/
+(require 'package)
+(add-to-list 'package-archives
+    '("marmalade" .
+      "http://marmalade-repo.org/packages/"))
+(package-initialize)
+
+(package-refresh-contents)
+
+(defvar my-packages
+  '(
+    clj-refactor
+    color-theme
+    dash
+    gh
+    gist
+    multiple-cursors
+    paredit
+    pkg-info
+    ruby-mode
+    s
+    smex
+    typopunct
+    yasnippet
+    ))
+
+(dolist (p my-packages)
+  (when (not (package-installed-p p))
+    (package-install p)))
