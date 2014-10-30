50595dffc2d4214d61158af22970c8fedee87d3c
Adam Tait <atait1@adamtait-dev-machine.local>
2014-10-30 00:42:33 -0700

    steal @holman's gitconfig setup

--- setup.sh
+++ setup.sh
@@ -44,6 +44,35 @@ fi
 if [[ ! -h ~/.mjolnir ]]; then
     ln -s $PWD/.mjolnir ~/
 fi
+if [[ ! -h ~/.gitconfig ]]; then
+    ln -s $PWD/.gitconfig ~/
+fi
+
+
+
+# gitconfig
+if [[ -f ~/.gitconfig ]]; then
+    echo "removing your old .gitconfig"
+    rm ~/.gitconfig
+fi
+if [[ ! -h ~/gitconfig.symlink ]]; then
+    echo 'creating a new gitconfig'
+
+    git_credential='cache'
+    if [ "$(uname -s)" == "Darwin" ]
+    then
+        git_credential='osxkeychain'
+    fi
+
+    echo ' - What is your github author name?'
+    read -e git_authorname
+    echo ' - What is your github author email?'
+    read -e git_authoremail
+
+    sed -e "s/AUTHORNAME/$git_authorname/g" -e "s/AUTHOREMAIL/$git_authoremail/g" -e "s/GIT_CREDENTIAL_HELPER/$git_credential/g" gitconfig.symlink > gitconfig.symlink
+
+    ln -s gitconfig.symlink ~/.gitconfig
+fi
 
 
 echo "adding OSX settings"
