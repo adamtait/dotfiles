5bc6e1dcf672ff869110aad3264b57318eeaa2c7
Adam Tait <adam@adamtait.com>
2015-05-11 00:07:26 -0700

    fix issues with emacs & gitignore be explicit about which directory is being used

--- setup.sh
+++ setup.sh
@@ -2,33 +2,67 @@
 
 CURRENT_DIR=$PWD
 DOTFILES_DIR=$0:a:h
-cd ~
+cd $DOTFILES_DIR
 
 echo "loading git submodules"
 git submodule init
 git submodule update
 
+if [[ ! -d $HOME ]]; then
+    echo "please set $HOME before proceeding"
+    exit 1
+fi
+
+if [[ ! -d /Applications/Emacs.app ]]; then
+    echo "XEmacs didn't get installed properly. Try running ./freshen.sh"
+    exit 1
+fi
+
+
+EMACS_EXEC=emacs
+if [[ `emacs -version` != "24" ]]; then
+    EMACS_EXEC="/Applications/Emacs.app/Contents/MacOS/Emacs -nw"
+
+    echo "Adding alias for command-line emacs"
+    touch $DOTFILES_DIR/zsh.d/emacs
+    echo "alias emacs=\"${EMACS_EXEC}\"" >> $DOTFILES_DIR/zsh.d/emacs
+    echo ">> you might want to: alias emacs=\"${EMACS_EXEC}\""
+fi
+
 echo "creating elpa directory & installing elpa packages"
-if [[ ! -d ~/.emacs.d ]]; then
+if [[ -d $HOME/.emacs.d ]]; then
     echo "removing old emacs configuration"
     # safety in the case that you had already installed .emacs configuration
-    sudo rm -rf ~/.emacs.d
+    sudo rm -rf $HOME/.emacs.d
 fi
-if [[ ! -h ~/.emacs.d ]]; then
-    ln -s $PWD/.emacs.d ~/
+if [[ ! -h $HOME/.emacs.d ]]; then
+    ln -s $DOTFILES_DIR/.emacs.d $HOME/
 fi
 
 vared -p "Would you like to install emacs elpa packages? (yes[y] or no[n]): " -c install_elpa
 if [[ ("$install_elpa" == "y") || ("$install_elpa" == "yes") ]]; then
     rm -rf $DOTFILES_DIR/.emacs.d/elpa
-    emacs --script "${DOTFILES_DIR}/install_elpa.el" &>/dev/null 
+
+    if [[ ! `emacs -version` =~ "24" ]]; then
+	if [[ -d /Applications/Emacs.app ]]; then
+	    #alias emacs="/Applications/Emacs.app/Contents/MacOS/Emacs -nw"
+	    echo "using /Applications/Emacs.app until zsh.d/emacs alias is setup correctly"
+	    /Applications/Emacs.app/Contents/MacOS/Emacs -nw --script "${DOTFILES_DIR}/install_elpa.el" #&>/dev/null 
+	else
+	    echo "please run ./freshen.sh to install XEmacs (& do lots of other things) before running setup.sh, again"
+	    exit 1
+	fi
+    else
+	$EMACS_EXEC --script "${DOTFILES_DIR}/install_elpa.el" #&>/dev/null 
+    fi
+    
     echo "\n\n"
     echo "DONE installing elpa packages"
 fi
 
 echo "creating .path"
 cat > "$HOME/.path" <<EOF
-$HOME/.dotfiles/bin
+$DOTFILES_DIR/.dotfiles/bin
 /usr/local/bin
 /opt/local/bin
 /usr/bin
@@ -39,7 +73,7 @@ $HOME/.dotfiles/bin
 /usr/texbin
 $(brew --prefix coreutils)/libexec/gnubin
 $JAVA_HOME/bin
-$PWD/submodules/bazel/output
+$DOTFILES_DIR/submodules/bazel/output
 EOF
 
 echo "creating .manpath"
@@ -49,32 +83,29 @@ EOF
 
 
 echo "setting up symlinks"
-if [[ ! -h ~/.zshrc ]]; then
-    ln -s $PWD/zshrc.symlink ~/.zshrc
+if [[ ! -h $HOME/.zshrc ]]; then
+    ln -s $DOTFILES_DIR/zshrc.symlink $HOME/.zshrc
 fi
-if [[ ! -d ~/.zsh.d ]]; then
-    mkdir ~/.zsh.d
-    ln -s $PWD/zsh.d/* ~/.zsh.d/
+if [[ ! -d $HOME/.zsh.d ]]; then
+    mkdir $HOME/.zsh.d
+    ln -s $DOTFILES_DIR/zsh.d/* $HOME/.zsh.d/
 fi
-if [[ ! -h ~/.emacs ]]; then
-    ln -s $PWD/emacs.symlink ~/.emacs
+if [[ ! -h $HOME/.emacs ]]; then
+    ln -s $DOTFILES_DIR/emacs.symlink $HOME/.emacs
 fi
-if [[ ! -h ~/.lein ]]; then
-    ln -s $PWD/.lein ~/.lein
+if [[ ! -h $HOME/.lein ]]; then
+    ln -s $DOTFILES_DIR/.lein $HOME/.lein
 fi
-if [[ ! -h ~/.mjolnir ]]; then
-    ln -s $PWD/.mjolnir ~/
-fi
-if [[ ! -h ~/.gitconfig ]]; then
-    ln -s $PWD/.gitconfig ~/
+if [[ ! -h $HOME/.mjolnir ]]; then
+    ln -s $DOTFILES_DIR/.mjolnir $HOME/
 fi
 
 # gitconfig
-if [[ (-f ~/.gitconfig) && (! -h ~/.gitconfig) ]]; then
+if [[ (-f $HOME/.gitconfig) && (! -h $HOME/.gitconfig) ]]; then
     echo "removing your old .gitconfig"
-    rm ~/.gitconfig
+    rm $HOME/.gitconfig
 fi
-if [[ ! -h ~/.gitconfig ]]; then
+if [[ ! -h $HOME/.gitconfig ]]; then
     echo 'creating a new .gitconfig'
 
     git_credential='cache'
@@ -84,11 +115,11 @@ if [[ ! -h ~/.gitconfig ]]; then
     fi
 
     vared -p ' - What is your github author name? ' -c git_authorname
-    vared -p ' - What is your github author email?' -c git_authoremail
+    vared -p ' - What is your github author email? ' -c git_authoremail
 
     sed -e "s/AUTHORNAME/$git_authorname/g" -e "s/AUTHOREMAIL/$git_authoremail/g" -e "s/GIT_CREDENTIAL_HELPER/$git_credential/g" gitconfig.template > gitconfig.symlink
 
-    ln -s $PWD/gitconfig.symlink ~/.gitconfig
+    ln -s $DOTFILES_DIR/gitconfig.symlink $HOME/.gitconfig
 fi
 
 
@@ -97,19 +128,19 @@ SUDO defaults write com.apple.Finder AppleShowAllFiles YES; killall Finder
 
 
 # install fonts
-if [[ ! -e ~/Library/Fonts/Anonymous\ Pro.ttf ]]; then
+if [[ ! -e $HOME/Library/Fonts/Anonymous\ Pro.ttf ]]; then
     echo "installing Anonymous Pro (emacs font)"
     cd /tmp
     curl http://www.marksimonson.com/assets/content/fonts/AnonymousPro-1.002.zip > /tmp/AnonymousPro-1.002.zip
     unzip /tmp/AnonymousPro-1.002.zip
-    cp /tmp/AnonymousPro-1.002.001/*.ttf ~/Library/Fonts/
+    cp /tmp/AnonymousPro-1.002.001/*.ttf $HOME/Library/Fonts/
     rm -rf /tmp/*AnonymousPro*
 fi
 
 # compile & install Bazel
-if [[ ! -f $PWD/submodules/bazel/output/bazel ]]; then
+if [[ ! -f $DOTFILES_DIR/submodules/bazel/output/bazel ]]; then
     echo "compiling Bazel"
-    sh $PWD/submodules/bazel/compile.sh
+    sh $DOTFILES_DIR/submodules/bazel/compile.sh
 fi
 
 
