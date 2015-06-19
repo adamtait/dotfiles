f066cd0bb5c9606d4b9503083ec617a5077c0e3a
Adam Tait <adam@adamtait.com>
2015-06-19 12:45:42 -0400

    add spyscope to lein profiles

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,9 +1,11 @@
 {:user {
-    :plugins [[cider/cider-nrepl "0.8.2"]
-              [lein-cprint "1.0.0"]
-              [lein-deps-tree "0.1.2"]
-	      [lein-jdk-tools "0.1.1"]
-	      [lein-marginalia "0.7.1"]
-	      [lein-test-bang-bang "0.2.0"]
-	      [org.clojars.strongh/lein-init-script "1.3.1"]
-	      [lein-localrepo "0.5.3"]]}}
+        :plugins [[cider/cider-nrepl "0.8.2"]
+                  [lein-cprint "1.0.0"]
+                  [lein-deps-tree "0.1.2"]
+                  [lein-jdk-tools "0.1.1"]
+                  [lein-marginalia "0.7.1"]
+                  [lein-test-bang-bang "0.2.0"]
+                  [org.clojars.strongh/lein-init-script "1.3.1"]
+                  [lein-localrepo "0.5.3"]]
+        :dependencies [[spyscope "0.1.5"]]
+        :injections [(require 'spyscope.core)]}}
