b440935e845f4612e211eb49bb3f8e73b2f811d2
Adam Tait <adam@adamtait.com>
2015-11-20 16:55:01 -0800

    move cider-nrepl to :repl profiles
    see http://blog.maio.cz/2015/11/cider-slows-down-leiningen-startup-here.html

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,6 +1,5 @@
 {:user {
-        :plugins [[cider/cider-nrepl "0.8.2"]
-                  [lein-cprint "1.0.0"]
+        :plugins [[lein-cprint "1.0.0"]
                   [lein-deps-tree "0.1.2"]
                   [lein-jdk-tools "0.1.1"]
                   [lein-marginalia "0.7.1"]
@@ -8,4 +7,5 @@
                   [org.clojars.strongh/lein-init-script "1.3.1"]
                   [lein-localrepo "0.5.3"]]
         :dependencies [[spyscope "0.1.5"]]
-        :injections [(require 'spyscope.core)]}}
+        :injections [(require 'spyscope.core)]}
+ :repl {:plugins [[cider/cider-nrepl "0.8.2"]]}}
