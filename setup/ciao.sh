cecb6f254a89ab77871095dfa0c583c93f796635
Adam Tait <adam@adamtait.com>
2018-09-24 18:50:27 -0700

    add CIAO setup script to symlink config file

--- setup/ciao.sh
+++ setup/ciao.sh
@@ -1,1 +1,22 @@
-../submodules/ciao/setup.sh
+#!/usr/bin/env bash
+
+echo -e "\n--- CIAO setup"
+
+DOTFILES_DIR=$(dirname $(realpath "$0"))/..
+CIAO_SCRIPT_DIR=$DOTFILES_DIR/submodules/ciao
+CONFIG_DIR=$DOTFILES_DIR/configuration/ciao
+CIAO_HOME_DIR=~/.ciao
+
+
+# run ciao setup
+sh $CIAO_SCRIPT_DIR/setup.sh
+
+
+# symlink .ciao/config
+if [[ -f $CIAO_HOME_DIR/config ]]; then
+    rm $CIAO_HOME_DIR/config
+fi
+
+
+echo -e "\n--- symlink ~/.ciao/config"
+ln -s $CONFIG_DIR/config $CIAO_HOME_DIR/config
