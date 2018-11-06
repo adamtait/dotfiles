703e2f4cc14873d1fff2004b387943e840325ee5
Adam Tait <bin@adamta.it>
2018-11-06 14:53:29 -0800

    remove backticks from echo command

--- install/2/gnu_utils.sh
+++ install/2/gnu_utils.sh
@@ -3,5 +3,5 @@
 echo -e "\n--- Install GNU core utilities (those that come with OS X are outdated)"
 brew install coreutils
 
-echo -e "\n--- Install GNU `find`, `locate`, `updatedb`, and `xargs`, g-prefixed"
+echo -e "\n--- Install GNU find, locate, updatedb, and xargs, g-prefixed"
 brew install findutils
