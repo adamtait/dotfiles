55a81eaebf7ede9d51d07bcb91761a94a12a71ce
tamthaopham <tamthaopham@gmail.com>
2019-01-18 22:50:08 -0800

    keybindings directory must exist before symlink

--- setup/osx_keybindings.sh
+++ setup/osx_keybindings.sh
@@ -2,10 +2,16 @@
 
 DOTFILES_DIR=$(dirname $(realpath "$0"))/..
 KB_FILE=DefaultKeyBinding.dict
-KB_PATH=$HOME/Library/KeyBindings/$KB_FILE
+KB_PATH=$HOME/Library/KeyBindings
+
+if [[ ! -d $KB_PATH ]]; then
+   echo ""
+   echo "--- creating $KB_PATH"
+   mkdir $KB_PATH
+fi
 
 if [[ ! -h $KB_PATH ]]; then
     echo ""
     echo "--- symlink $KB_FILE"
-    ln -s $DOTFILES_DIR/configuration/osx/$KB_FILE $KB_PATH
+    ln -s $DOTFILES_DIR/configuration/osx/$KB_FILE $KB_PATH/
 fi
