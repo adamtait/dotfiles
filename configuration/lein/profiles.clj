b23385916829f7e010f122251b6b70078cff88be
Adam Tait <bin@adamta.it>
2019-07-10 20:25:05 -0700

    update lein profiles package versions

--- configuration/lein/profiles.clj
+++ configuration/lein/profiles.clj
@@ -1,11 +1,11 @@
 {:user {
-        :plugins [[lein-cprint "1.0.0"]
+        :plugins [[lein-cprint "1.3.1"]
                   [lein-deps-tree "0.1.2"]
                   [lein-jdk-tools "0.1.1"]
-                  [lein-marginalia "0.7.1"]
+                  [lein-marginalia "0.9.1"]
                   [lein-test-bang-bang "0.2.0"]
-                  [org.clojars.strongh/lein-init-script "1.3.1"]
-                  [lein-localrepo "0.5.3"]]
-        :dependencies [[spyscope "0.1.5"]]
+                  [org.clojars.strongh/lein-init-script "1.4.0"]
+                  [lein-localrepo "0.5.4"]]
+        :dependencies [[spyscope "0.1.6"]]
         :injections [(require 'spyscope.core)]}
- :repl {:plugins [[cider/cider-nrepl "0.8.2"]]}}
+ :repl {:plugins [[cider/cider-nrepl "0.21.1"]]}}
