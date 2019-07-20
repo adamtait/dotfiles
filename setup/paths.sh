6b59dc69772a667ab0ab9bb09a7e08b5c743720a
Adam Tait <bin@adamta.it>
2019-07-20 16:00:55 -0700

    reorder paths so that our additions are searched first

--- setup/paths.sh
+++ setup/paths.sh
@@ -5,19 +5,19 @@ DOTFILES_DIR=$(dirname $(realpath "$0"))/..
 echo ""
 echo "--- creating ~/.path"
 cat > "$HOME/.path" <<EOF
-/bin
-/usr/bin
-/sbin
-/usr/sbin
-/opt/X11/bin
-/usr/texbin
 $(brew --prefix coreutils)/libexec/gnubin
-/usr/local/bin
-/opt/local/bin
-/usr/local/opt/libarchive/bin
 $DOTFILES_DIR/bin
 $DOTFILES_DIR/submodules/bazel/output
 $HOME/workspace/google-cloud-sdk/bin
+/usr/local/bin
+/opt/local/bin
+/usr/local/opt/libarchive/bin
+/opt/X11/bin
+/usr/texbin
+/usr/sbin
+/usr/bin
+/sbin
+/bin
 EOF
 
 echo ""
