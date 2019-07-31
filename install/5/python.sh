8cf14cf61e9a67012aff2b973e8c49c67c753f42
Adam Tait <bin@adamta.it>
2019-07-30 18:23:05 -0700

    update python installer
    + I think we might want to use brew, next time

--- install/5/python.sh
+++ install/5/python.sh
@@ -2,4 +2,21 @@
 
 echo ""
 echo "--- Python lang"
-pip install --upgrade pip setuptools
+
+
+DEST_DIR=/tmp
+FILENAME="python-3.7.4-macosx10.6.pkg"
+START_DIR=`pwd`
+
+echo "---- downloading..."
+curl -0L https://www.python.org/ftp/python/3.7.4/$FILENAME > $DEST_DIR/$FILENAME
+
+cd $DEST_DIR
+
+echo "---- installing Python 3.7.4 package (requires root)"
+sudo installer -pkg  $FILENAME -target /
+rm -rf $FILENAME
+
+cd $START_DIR
+
+
