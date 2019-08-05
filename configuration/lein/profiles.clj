e838fed679700bf6d21ed2aba77c43570a0f3445
Adam Tait <bin@adamta.it>
2019-08-05 13:48:11 -0700

    add latest cider-nrepl to lein profiles

--- configuration/lein/profiles.clj
+++ configuration/lein/profiles.clj
@@ -5,7 +5,8 @@
                   [lein-marginalia "0.9.1"]
                   [lein-test-bang-bang "0.2.0"]
                   [org.clojars.strongh/lein-init-script "1.4.0"]
-                  [lein-localrepo "0.5.4"]]
+                  [lein-localrepo "0.5.4"]
+                  [cider/cider-nrepl "0.21.1"]]
         :dependencies [[spyscope "0.1.6"]]
         :injections [(require 'spyscope.core)]}
  :repl {:plugins [[cider/cider-nrepl "0.21.1"]]}}
