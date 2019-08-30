7eac0b1c30a56762137bb7e21be928dd3a394784
Adam Tait <bin@adamta.it>
2019-08-30 13:14:32 -0700

    update dnscrypt script to handle different service request types

--- bin/dnscrypt-stop.sh
+++ bin/dnscrypt-stop.sh
@@ -1,3 +1,13 @@
 #!/usr/bin/env bash
 
-sudo dnscrypt-proxy -config ~/.dnscrypt/dnscrypt-proxy.toml -service stop
+# check usage
+if [ "$1" != "start" ] || [ "$1" != "stop" ] || [ "$1" != "restart" ]; then
+    echo ""
+    echo "--- dnscrypt-service ---"
+    echo "Usage: dnscrypt-service [start|stop|restart]"
+    echo "example: dnscrypt-service stop"
+    exit 0
+fi
+
+
+sudo dnscrypt-proxy -config ~/.dnscrypt/dnscrypt-proxy.toml -service $1
