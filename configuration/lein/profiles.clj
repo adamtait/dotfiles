547a2bab9f93fe8ab56f5f2e62d076bba84b5a8f
Adam Tait <bin@adamta.it>
2020-03-09 19:23:13 -0700

    clean up lein profiles

--- configuration/lein/profiles.clj
+++ configuration/lein/profiles.clj
@@ -1,10 +1,17 @@
-{:user {
-        :plugins [[lein-cprint "1.3.2"]
-                  [lein-deps-tree "0.1.2"]
-                  [lein-tools-deps "0.4.5"]
-                  [lein-marginalia "0.9.1"]
-                  [org.clojars.strongh/lein-init-script "1.4.0"]
-                  [cider/cider-nrepl "0.24.0"]]
-        :dependencies [[spyscope "0.1.6"]]
-        :injections [(require 'spyscope.core)]}
- :repl {:plugins [[cider/cider-nrepl "0.24.0"]]}}
+{
+ :user
+ {
+  :dependencies
+  [[org.clojure/tools.namespace "1.0.0"]
+
+   [spyscope "0.1.6"]
+   [io.aviso/pretty "0.1.37"
+    :exclusions [org.clojure/clojure]]]
+
+  ;; magic
+  :plugins
+  [[cider/cider-nrepl "0.24.0"]
+   [io.aviso/pretty "0.1.37"]]
+  
+  :middleware     [io.aviso.lein-pretty/inject]
+  :injections     [(require 'spyscope.core)]}}
