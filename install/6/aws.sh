881689153ae01136110a120ab9922edb62e7d765
Adam Tait <bin@adamta.it>
2021-05-27 22:47:16 -0700

    install/6/aws: update to AWS CLI V2

--- install/6/aws.sh
+++ install/6/aws.sh
@@ -6,19 +6,13 @@ echo "--- Amazon Web Service CLI"
 
 
 DEST_DIR=/tmp
-FILENAME="awscli-bundle.zip"
-START_DIR=`pwd`
+FILENAME="AWSCLIV2.pkg"
 
 
 echo "---- downloading..."
-curl "https://s3.amazonaws.com/aws-cli/$FILENAME" -o "$DEST_DIR/$FILENAME"
+curl "https://awscli.amazonaws.com/$FILENAME" -o "$DEST_DIR/$FILENAME"
 
-cd $DEST_DIR
 
-echo "---- running installer. installing to /usr/local/aws with symlinks in /usr/local/bin/aws"
-unzip $FILENAME
-sudo ./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
-
-rm -rf $FILENAME awscli-bundle
-
-cd $START_DIR
+echo "---- running installer"
+sudo installer -pkg $DEST_DIR/$FILENAME -target /
+rm -rf $DEST_DIR/$FILENAME
