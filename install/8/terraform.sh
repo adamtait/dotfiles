2cad6c114e34ae2f5d5906873ce2f2a4b67ef6ca
adamtait <bin@adamta.it>
2022-02-24 23:02:10 -0800

    install/terraform: use homebrew

--- install/8/terraform.sh
+++ install/8/terraform.sh
@@ -1,24 +1,10 @@
 #!/usr/bin/env bash
 
-DEST_DIR=/usr/local/bin
-TMP_DIR=/tmp
-VERSION=0.12.21
-FILENAME=terraform_${VERSION}_darwin_amd64.zip
-OLD_PWD=`pwd`
-
 echo ""
 echo "--- TerraForm"
 echo ""
-echo "---- downloading..."
 
-cd $TMP_DIR
-curl -L0 https://releases.hashicorp.com/terraform/${VERSION}/$FILENAME > $FILENAME
-unzip $FILENAME
-rm -f $FILENAME
+brew tap hashicorp/tap
+brew install hashicorp/tap/terraform
 
-echo ""
-echo "---- installing to ${DEST_DIR}"
-mv terraform $DEST_DIR/
-
-cd $OLD_PWD
 echo "fin."
