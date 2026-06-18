6d2c072d995cb87bfb8c582d507994213c827ecb
Adam Tait <bin@adamta.it>
2026-06-18 12:06:05 -0700

    Revert "Add 74-ruby.zsh conf.d module for rbenv init (#11)"
    This reverts commit ae87d887a7915a94a5a60129f62cd41da4c77bbb.

--- home/dot_config/zsh/conf.d/74-ruby.zsh
+++
@@ -1,8 +0,0 @@
-# Ruby via rbenv.
-
-export RBENV_ROOT="$HOME/.rbenv"
-[[ -d "$RBENV_ROOT/bin" ]] && path=("$RBENV_ROOT/bin" $path)
-
-if (( $+commands[rbenv] )); then
-  eval "$(rbenv init - zsh)"
-fi
